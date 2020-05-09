365b24f188509d429feba9e9735e871e3971dfff
Devanshi <devanshishah408@gmail.com>
2020-05-09 11:05:33 -0700

    step 3: init function

--- finalexam-app/src/app/app.component.ts
+++ finalexam-app/src/app/app.component.ts
@@ -8,6 +8,29 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'finalexam-app';
 
+  start: number;
+  end: number;
+  numberSubmitted: boolean;
+  evenNumbers: Array<any> = [];
+  submit(start, end){
+    this.numberSubmitted = true;
+    if(start > end) {
+      let temp;
+      temp = start;
+      start = end;
+      end = temp;
+    }
+    for( let i=start; i<=end; i++){
+      let result = this.isEven(i)
+      if(result) {this.evenNumbers.push(i)}
+    }
+  }
+
+  isEven(value) {
+    if (value%2 == 0)
+      return true;
+    else
+      return false;
 
 
   }
